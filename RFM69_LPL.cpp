d47c0deea5755dc2c4208e98a5dc9de9b798828e
TrystanLea <trystan.lea@gmail.com>
2023-04-04 16:23:28 +0100

    rename so that version can be installed alongside original or other library called RFM69

--- RFM69_LPL.cpp
+++ RFM69_LPL.cpp
@@ -23,8 +23,8 @@
 // Please maintain this license information along with authorship
 // and copyright notices in any redistribution of this code
 // **********************************************************************************
-#include "RFM69.h"
-#include "RFM69registers.h"
+#include "RFM69_LPL.h"
+#include "RFM69_LPL_registers.h"
 
 uint8_t RFM69::DATA[RF69_MAX_DATA_LEN+1];
 uint8_t RFM69::_mode;        // current transceiver state
